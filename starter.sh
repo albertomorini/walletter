2bbf5ee233018b5f2e03b1512a4fab2e74de88be
albertomorini <99morini@gmail.com>
2023-06-18 20:14:28 +0200

    fixed starter script + general improvements

--- starter.sh
+++ starter.sh
@@ -8,7 +8,7 @@ docker run -d -p 27017:27017 --restart=unless-stopped --name myMongo -v mongo-da
 ## start client
 cd
 cd dev/Walletter/Walletter
-npx serve -s build --listen 3000 --ssl-cert cert.pem --ssl-key key.pem &
+npx serve -s build --listen 3000 --ssl-cert ./cert/cert.pem --ssl-key ./cert/key.pem &
 
 
 ## start server
