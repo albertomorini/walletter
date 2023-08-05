53b63acafe249767c8a2d36122b7029e01c14330
albertomorini <99morini@gmail.com>
2023-08-05 10:30:23 +0200

    fixed shell started

--- starter.sh
+++ starter.sh
@@ -6,14 +6,12 @@ docker run -d -p 27017:27017 --restart=unless-stopped --name myMongo -v mongo-da
 
 
 ## start client
-cd
 cd ./Walletter
 npx serve -s build --listen 1998 --ssl-cert ./cert/cert.pem --ssl-key ./cert/key.pem &
 
 
 ## start server
-
-cd 
+cd ..
 cd ./server
 node server.js &
 
